3f57f4dd438b0e9b86bda3a5b9cb2229f3c990f6
SureshManiDevendran <sureshmani311@gmail.com>
2026-06-02 09:58:36 +0530

    Worked on the Custom Commands in Django

---
+++ AWD/dataentry/management/commands/greetings.py
@@ -0,0 +1,15 @@
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    help="This will display the greeting command..."
+
+    def add_arguments(self, parser):
+        parser.add_argument('name',type=str,help='This Specifies User Name') 
+
+
+    def handle(self, *args, **kwargs):
+        try:
+           name=kwargs['name']
+           self.stdout.write(self.style.SUCCESS(f"Hi {name}"))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(str(e)))
