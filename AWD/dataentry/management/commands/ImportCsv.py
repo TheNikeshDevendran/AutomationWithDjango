6d4f717d5bcbb3ed95a3e69136d08a0a1423ceb2
Nikesh Devendran <nikeshdevendran07@gmail.com>
2026-06-02 18:47:14 +0530

    Implenting the search feature for Model so that we can inset the data in the appropiate Table

--- AWD/dataentry/management/commands/ImportCsv.py
+++ AWD/dataentry/management/commands/ImportCsv.py
@@ -1,18 +1,32 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand,CommandError
 import csv
+from django.apps import apps
 from dataentry.models import Students
 class Command(BaseCommand):
     def add_arguments(self,parser):
         parser.add_argument('file_path',type=str,help="CSV file path for importing data")
+        parser.add_argument('modelname',type=str,help="Table name for inserting the data")
+
     
     def handle(self,*args,**kwargs):
         file_path=kwargs['file_path']
+        modelName=kwargs['modelname'].capitalize()
+        model=None
+        for app_config in apps.get_app_configs():
+            try:
+                model=apps.get_model(app_config.label,modelName)
+                break
+            except LookupError:
+                continue
+        if not model:
+            raise CommandError(f"{modelName} not found in the apps")
+    
         with open(file_path,'r') as file:
             reader=csv.DictReader(file)
             for row in reader:
                 exists=Students.objects.filter(roll_no=row['roll_no']).exists()
                 if not exists:
-                    Students.objects.create(**row)
+                    model.objects.create(**row)
                     self.stdout.write(self.style.SUCCESS('Data inserted Successfully'))
                 else:
                     self.stdout.write(self.style.WARNING(f'Data with rollno{row['roll_no']} already present'))
