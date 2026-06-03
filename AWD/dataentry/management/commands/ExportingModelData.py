1eb1d812eedf8e5b92b26fbac41c9e834da458ba
Nikesh Devendran <nikeshdevendran07@gmail.com>
2026-06-03 08:45:01 +0530

    Worked on the Exporting Model data in CSV

--- AWD/dataentry/management/commands/ExportingModelData.py
+++ AWD/dataentry/management/commands/ExportingModelData.py
@@ -1,16 +1,36 @@
 from django.core.management.base import BaseCommand
-from dataentry.models import Students
+# from dataentry.models import Students
 import datetime
 import csv
+from django.apps import apps
 class Command(BaseCommand):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
-    file_path=f"exportedStudent_{timestamp}.csv"
-    def handle(self, *args, **options):
-        students=Students.objects.all()
-        with open(self.file_path,'w',newline='') as file:
+   
+
+    def add_arguments(self, parser):
+        parser.add_argument('modelname',type=str,help='Model/Tabel name is required for exporting the data')
+    def handle(self, *args, **kwargs):
+        model_name=kwargs['modelname'].capitalize()
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+        file_path=f"exported_{model_name}_{timestamp}.csv"
+        
+        model=None
+        for appconfig in apps.get_app_configs():
+            try:
+               model=apps.get_model(appconfig.label,model_name)
+               break
+            except LookupError:
+                pass
+        
+        if not model:
+            self.stdout.write(self.style.ERROR(f'{model_name} not found'))
+            return 
+        data=model.objects.all()
+        with open(file_path,'w',newline='') as file:
             CSVwriter=csv.writer(file)
-            CSVwriter.writerow(['RollNO','Name','Age'])
-            for student in students:
-                CSVwriter.writerow([student.roll_no,student.name,student.age])
+            # it will give us the column name/headers
+            CSVwriter.writerow([field.name for field in model._meta.fields])
+
+            for dt in data:
+                CSVwriter.writerow([getattr(dt,field.name) for field in model._meta.fields])
         self.stdout.write(self.style.SUCCESS("Data Exported Sucessfully"))
         
